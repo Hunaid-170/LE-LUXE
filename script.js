4815ba0f9c75cb37715f829c33028cd09e16c1bb
Hunaid-170 <hunaidaptechgmail.com>
2026-04-30 16:45:47 +0500

    Updated product page design

--- script.js
+++ script.js
@@ -98,3 +98,11 @@ $(document).ready(function(){
   });
 
 });
+
+
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', e => {
+  glow.style.left = e.clientX + 'px';
+  glow.style.top = e.clientY + 'px';
+});
